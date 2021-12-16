2a8ffa7ac929a56d79aa0f7ee3c39d2e441d5aed
Grogu <Grogu@put-the-red-wire-where-blue-one-was>
2021-12-16 22:40:35 +0300

    we share a level and than there is one beyond

--- program.sh
+++ program.sh
@@ -7,8 +7,14 @@ main(){
 compile(){
   rm -rf ./out
   qmake -o out/Makefile program.pro
-  cd out
+  cd ./out
   make
+  cd ../
+}
+
+release(){
+  compile
+  mv out/Grogu out/Grogu-$(git rev-parse --short HEAD)-linux64
 }
 
 "$@"
